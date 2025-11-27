5f0b79b0b4303a43ffd894a122e8d53e0f7863c5
Vaduva <vaduvaa309@gmail.com>
2025-11-27 11:54:03 +0200

    Added postgre <not finished>

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,13 @@
 from flask import Flask , make_response , jsonify
 from auth import auth_bp
-
+from .extensions import db
+from .config import Config
 app = Flask(__name__)
 
 app.register_blueprint(auth_bp)
+app.config.from_object(Config)
+
+db.init_app()
 
 if __name__ == "__main__":
     app.run()
